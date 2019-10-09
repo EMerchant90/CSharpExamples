5d3acd2b42361a97b03e34e774a3c2d6a096f2a1
Ejaz <ejaz.merchant@icloud.com>
2019-10-08 22:31:19 -0400

    Class account/interestAccount subclass challenge

---
+++ MethodsAndStructs/MethodsAndStructs/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndStructs
+{
+    class Account
+    {
+        public string accountType { get; set; }
+        public bool isActive;
+        public decimal Amount { get; set; }
+    }
+}
